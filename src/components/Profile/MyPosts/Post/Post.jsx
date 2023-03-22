7223f015c38f392dfa4d5fede3272779db7f8277
Mikhail Lavrov <lavrov.fd@gmail.com>
2023-03-22 13:44:27 +0300

    fix/ myposts likes

--- src/components/Profile/MyPosts/Post/Post.jsx
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -1,15 +1,27 @@
+import { useState } from 'react';
 import { useSelector } from 'react-redux';
 import c from './Post.module.scss';
 
 const Post = ({id, message, likeCounter}) => {
   const avatar = useSelector((state) => state.auth.avatarImg);
+  const [likes, setLikes] = useState(likeCounter);
+  const [liked, setLiked] = useState(false);
+
+  const handleLikeToggle = () => {
+    liked ? setLikes(likes - 1) : setLikes(likes + 1);
+    setLiked(!liked);
+  }
 
   return (
-      <div className={c.post} key={id}>
-        <img src={avatar} alt="ava" />
-        {message}
-        <span className={c.post__likecounter}>&#9825; {likeCounter}</span>
-      </div>
+    <div className={c.post} key={id}>
+      <img src={avatar} alt="ava" />
+      {message}
+      <button className={c.post__likecounter} 
+              type='button' 
+              onClick={handleLikeToggle}>
+        {liked ? '❤️' : '🤍'} {likes}
+      </button>
+    </div>
   );
 }
 
